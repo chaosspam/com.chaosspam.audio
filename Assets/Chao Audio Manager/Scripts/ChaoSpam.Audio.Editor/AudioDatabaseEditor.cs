28fb8133b2858eb33716bed8147b5cc8da654fde
Chao <chaohsulspam@gmail.com>
2025-08-16 18:08:41 +0800

    feat: move editor scripts to other assembly

---
+++ Assets/Chao Audio Manager/Scripts/ChaoSpam.Audio.Editor/AudioDatabaseEditor.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+
+namespace ChaosSpam.Audio.Editor
+{
+    [CustomEditor(typeof(AudioDatabase))]
+    public class AudioDatabaseEditor : UnityEditor.Editor
+    {
+        private SoundImport obj;
+        private SerializedObject soundImport;
+        private SerializedProperty template;
+        private SerializedProperty clips;
+
+        private void OnEnable()
+        {
+            obj = ScriptableObject.CreateInstance<SoundImport>();
+            soundImport = new SerializedObject(obj);
+            template = soundImport.FindProperty("template");
+            clips = soundImport.FindProperty("clips");
+        }
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            AudioDatabase database = (AudioDatabase)target;
+
+            if (GUILayout.Button("Sort"))
+            {
+                database.SortMusic();
+                database.SortSounds();
+                EditorUtility.SetDirty(database);
+            }
+
+            EditorGUILayout.PropertyField(template, new GUIContent("Template"));
+            EditorGUILayout.PropertyField(clips, new GUIContent("Clips"));
+
+            if (GUILayout.Button("Add Sounds"))
+            {
+                int start = database.soundList.Length;
+                Array.Resize(ref database.soundList, database.soundList.Length + obj.clips.Length);
+                for (int i = 0; i < obj.clips.Length; i++)
+                {
+                    AudioClip clip = obj.clips[i];
+                    Sound s = new Sound(obj.template, clip);
+                    database.soundList[start + i] = s;
+                }
+                EditorUtility.SetDirty(database);
+            }
+
+            soundImport.ApplyModifiedProperties();
+        }
+    }
+
+    [Serializable]
+    public class SoundImport : ScriptableObject
+    {
+        public Sound template;
+        public AudioClip[] clips;
+    }
+}
---
+++ Assets/Chao Audio Manager/Scripts/ChaoSpam.Audio/Data/AudioDatabase.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace ChaosSpam.Audio
+{
+    [CreateAssetMenu(fileName = "Audio Database", menuName = "Audio/Audio Database")]
+    public class AudioDatabase : ScriptableObject
+    {
+        public Music[] musicList;
+        public Sound[] soundList;
+
+#if UNITY_EDITOR
+        public void SortMusic()
+        {
+            System.Array.Sort(musicList);
+        }
+        public void SortSounds()
+        {
+            System.Array.Sort(soundList, (Sound a, Sound b) => a.name.CompareTo(b.name));
+        }
+#endif
+    }
+}
