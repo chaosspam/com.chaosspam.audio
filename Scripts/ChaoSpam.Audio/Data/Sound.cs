eb5ff49c973dd3e3ebb23e1166e6fcf1efeb0d25
Chao <chaohsulspam@gmail.com>
2025-08-16 18:08:41 +0800

    feat: move editor scripts to other assembly

---
+++ Scripts/ChaoSpam.Audio/Data/Sound.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+namespace ChaosSpam.Audio
+{
+    [System.Serializable]
+    public class Sound
+    {
+        public string name;
+        public AudioClip clip;
+        public AudioMixerGroup output;
+        [Range(0f, 1f)]
+        public float volume = 1f;
+        [Range(-3f, 3f)]
+        public float basePitch = 1f;
+        public float pitchFluctuation = 0f;
+
+        public bool loop;
+        public bool oneShot;
+        public bool playOnPause;
+
+        public Sound(Sound template, AudioClip clip)
+        {
+            this.name = clip.name;
+            this.clip = clip;
+            this.output = template.output;
+            this.volume = template.volume;
+            this.basePitch = template.basePitch;
+            this.pitchFluctuation = template.pitchFluctuation;
+            this.loop = template.loop;
+            this.oneShot = template.oneShot;
+            this.playOnPause = template.playOnPause;
+        }
+    }
+
+    public class SoundPlayer
+    {
+        private Sound sound;
+        private AudioSource source;
+        public bool playedThisFrame;
+
+        private float randomPitch
+        {
+            get => sound.pitchFluctuation == 0
+                ? sound.basePitch
+                : Random.Range(sound.basePitch - sound.pitchFluctuation, sound.basePitch + sound.pitchFluctuation);
+        }
+
+        public SoundPlayer(SoundManager soundManager, Sound sound)
+        {
+            this.sound = sound;
+
+            if (sound.oneShot && sound.pitchFluctuation == 0f)
+            {
+                source = soundManager.oneShotSource;
+            }
+            else
+            {
+                source = soundManager.manager.gameObject.AddComponent<AudioSource>();
+                source.loop = sound.loop;
+            }
+
+            source.clip = sound.clip;
+            source.volume = sound.volume;
+            source.pitch = sound.basePitch;
+            source.outputAudioMixerGroup = sound.output;
+        }
+
+        public void Play(float? pitch, bool bypassRepeat)
+        {
+            if (source == null) return;
+            if (Time.timeScale == 0f && !sound.playOnPause) return;
+
+            float pitchToPlay = pitch ?? randomPitch;
+
+            // Prevent multiple sounds from playing on the same frame
+            if (playedThisFrame && !bypassRepeat) return;
+            playedThisFrame = true;
+
+            if (sound.oneShot)
+            {
+                if (sound.pitchFluctuation != 0f)
+                {
+                    source.pitch = pitchToPlay;
+                }
+                source.outputAudioMixerGroup = sound.output;
+                source.PlayOneShot(sound.clip);
+            }
+            else
+            {
+                if (source.timeSamples == 0 || source.timeSamples >= sound.clip.samples)
+                {
+                    source.pitch = pitchToPlay;
+                    source.Play();
+                }
+            }
+        }
+    }
+}
